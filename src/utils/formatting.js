ad36235826b2b6a7c17ed4bb0815e17b6d862cdd
Nicko254 <nichlarsen@gmail.com>
2025-01-28 11:56:55 +1300

    Update formatting.js

--- src/utils/formatting.js
+++ src/utils/formatting.js
@@ -1,1 +1,23 @@
+export const formatCurrency = (number) => {
+    return number.toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    });
+};
 
+export const formatPercentage = (number) => {
+    return number.toLocaleString('en-US', {
+        style: 'percent',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    });
+};
+
+export const formatNumber = (number) => {
+    return number.toLocaleString('en-US', {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    });
+};
